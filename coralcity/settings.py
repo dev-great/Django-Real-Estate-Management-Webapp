d71c19e015133b4c44bad201e26577b418828578
Marshal Great <31458994+GR8T@users.noreply.github.com>
2021-07-08 12:36:24 +0100

    Update settings.py

--- coralcity/settings.py
+++ coralcity/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '+=mb2q!t+yg7(m$!_$iki#2*z(+ub^lcas0jx$l2-dp%bp8pt)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sheltered-dawn-17012.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['sheltered-dawn-17012.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
