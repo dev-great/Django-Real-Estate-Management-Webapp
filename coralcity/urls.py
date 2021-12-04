4f523824c363f885241d4209df262d7efbbf1fa9
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2021-12-04 14:09:14 +0100

    Update urls.py

--- coralcity/urls.py
+++ coralcity/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
-admin.site.site_header ="Coralcity control panel"
+admin.site.site_header ="Koralcity control panel"
 admin.site.index_title ="Administrators dashboard"
 admin.site.site_title ="Control panel"
 
